5467db98ebf7054468f8c1e17785e25e99122838
taidixiong <351095656@qq.com>
2025-07-06 16:27:25 +0800

    feat(base): 基础中间件 (#1)
    Co-authored-by: xiongyanfei <xiongyanfei@dp.tech>

--- service/pkg/web/routes.go
+++ service/pkg/web/routes.go
@@ -3,6 +3,7 @@ package web
 import (
 	"fmt"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/scienceol/studio/service/internal/configs/webapp"
 	"github.com/scienceol/studio/service/pkg/web/views"
@@ -32,6 +33,7 @@ func installMiddleware(g *gin.Engine) {
 	// TODO: trace 中间件
 	g.ContextWithFallback = true
 	server := webapp.Config().Server
+	g.Use(cors.Default())
 	g.Use(otelgin.Middleware(fmt.Sprintf("%s-%s",
 		server.Platform,
 		server.Service)))
