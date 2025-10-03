5f11cd32dbad2c21cc4cecae133eeff747dc9a69
HarveyQue <harveyqueque@outlook.com>
2025-10-03 19:02:26 +0800

    feat: Integrate useXyzen for dynamic panel width adjustment in App component

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,7 +1,8 @@
-import { Xyzen } from '@sciol/xyzen';
+import { useXyzen, Xyzen } from '@sciol/xyzen';
 import { useNavigate } from 'react-router-dom';
 
 export default function App() {
+  const { panelWidth } = useXyzen();
   const navigate = useNavigate();
 
   const handleGoLogin = () => {
@@ -9,16 +10,21 @@ export default function App() {
   };
 
   return (
-    <main className="h-full w-full flex flex-col items-center justify-center gap-6">
-      <div className="font-bold bg-black text-white p-4 rounded">
-        Studio 简易主页
-      </div>
-      <button
-        className="px-4 py-2 bg-blue-600 text-white rounded shadow"
-        onClick={handleGoLogin}
+    <main className="flex h-full">
+      <div
+        style={{ width: `calc(100% - ${panelWidth}px)` }}
+        className="h-full mt-20 flex flex-col items-center justify-center gap-6"
       >
-        跳转到登录页
-      </button>
+        <div className="font-bold bg-black text-white p-4 rounded">
+          Studio 简易主页
+        </div>
+        <button
+          className="px-4 py-2 bg-blue-600 text-white rounded shadow"
+          onClick={handleGoLogin}
+        >
+          跳转到登录页
+        </button>
+      </div>
       <Xyzen />
     </main>
   );
